baaa30f5ae02ceca331e0ff870f1199f25079ff5
Gemechisgk <gemechis.gk@gmail.com>
2023-01-04 09:22:11 +0300

    fix my code challenge

--- 0x01-Fix_My_Code_Challenge/status_server/api/v1/app.py
+++ 0x01-Fix_My_Code_Challenge/status_server/api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Web server 
+Web server
 """
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
@@ -16,5 +16,5 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    # python -m api.v1.app 
+    # python -m api.v1.app
     app.run(host="0.0.0.0", port=5000)
